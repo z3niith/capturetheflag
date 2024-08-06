b75d604433abb4d7f6af5b3dbd0fcb78dcb4fa61
germain001 <elengegermain08@hotmail.com>
2024-08-06 13:59:21 -0400

    fixes???

--- decrypt.py
+++ decrypt.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     encrypted_flag = "FWI{fdhvdu_vkliw_fkdoohqjh}"
     print("Encrypted Flag:", encrypted_flag)
 
-    # Decrypt by reversing the shift (Hint for the player)
-    # Explore how Python’s indexing or slicing might work with negative numbers. How could -3 play a role in this context? 
-    # decrypted_flag = ''.join([chr(ord(c) - 3) for c in encrypted_flag])
-    # print("Decrypted Flag:", decrypted_flag)
+    # Decrypt by reversing the shift
+    decrypted_flag = ''.join([chr(ord(c) - 3) for c in encrypted_flag])
+    print("Decrypted Flag:", decrypted_flag)
