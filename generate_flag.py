ab2d4a3196bf76177eec979eab2cdfbe4a19d17d
germain001 <elengegermain08@hotmail.com>
2024-08-06 15:30:41 -0400

    hot fixes and improvements

--- generate_flag.py
+++ generate_flag.py
@@ -8,7 +8,7 @@ def generate_flag_file():
     with open("link.txt", "w") as f:
         f.write(flag)
 
-    print("Flag file generated: link.txt")
+    print("Flag file link generated: link.txt")
 
 if __name__ == "__main__":
     generate_flag_file()
