7f84795e001e8d8458ca02f621b7d8cd94f8ba8d
germain001 <elengegermain08@hotmail.com>
2024-11-12 10:26:14 -0500

    Finale and end of the project.

--- decrypt_2.py
+++ decrypt_2.py
@@ -1,10 +1,10 @@
 def encrypt_flag(flag):
-    encrypted = ''.join([chr(ord(c) + 3) for c in flag])
+    encrypted = ''.join([chr(ord(c) - 21) for c in flag])
     return encrypted
 
 if __name__ == "__main__":
 
-    encrypted_flag = "Frghlqwkhvfkrrov"
+    encrypted_flag = "Qfxy_Tsj"
     print("Encrypted Flag:", encrypted_flag)
 
 
